2aa5a3aa1d00c387f922e64907da828247ba0529
6KeItA9 <norkuskeita7@gmail.com>
2024-12-09 13:20:23 +0200

    Add files via upload

---
+++ 12 ned.py
@@ -0,0 +1,12 @@
+kaulinu_metieni = [(3, 6)(4, 5)(6, 6)(1, 3)(2, 2)
+                   (4, 4)(5, 3)(2, 6)(1, 5)(6, 3)]
+lielaka_suma = 0
+
+print ("Kauliņu metieni:")
+for i, (kaulins1, kaulins2)in enumerate (kaulinu metieni, start = 1):
+    summa = kaulins1 + kaulins2
+    print (f"{i}. {kaulins1} {kaulins2} {summa}")
+    lielaka_summa = max (lielaka_suma, summa)
+    
+    print (f"Lielākā summa: {lielaka_suma}")
+    
